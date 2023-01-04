acd82a534d6f96acd1fe47dcd8aaa0df1f00df7d
damolabolaji <bolaji.oyindamola@gmail.com>
2023-01-04 14:21:14 +0100

    feat: added banner button with active & hover states

--- components/banner.js
+++ components/banner.js
@@ -3,13 +3,21 @@ import Image from "next/image";
 
 function banner() {
   return (
-    <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+    <div className='flex items-center relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
       <Image
         src='/images/the-anam.jpg'
         alt='banner image'
         layout='fill'
         objectFit='cover'
       />
+      <div className='absolute w-full top-1/2 text-center bg-white bg-opacity-60 p-10'>
+        <p className='text-sm text-red-500 sm:text-xl p-5 font-bold'>
+          Not sure where to go? Perfect!
+        </p>
+        <button className='text-gray-600 px-10 py-4 rounded-full bg-white shadow-md font-bold hover:shadow-xl active:scale-90 transition duration-150'>
+          I'm flexible
+        </button>
+      </div>
     </div>
   );
 }
